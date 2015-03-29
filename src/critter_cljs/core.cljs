6273b1b48af7dafd3c250cfe46f5a7bf5d2e22a3
Justin Falcone <stanulam59@gmail.com>
2015-03-29 15:29:57 -0400

    rules

--- src/critter_cljs/core.cljs
+++ src/critter_cljs/core.cljs
@@ -7,8 +7,14 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defn init-critters []
-    (let [names ["Slipper" "Allegra" "Squeaky" "Sarah Jane" "Totoro"]]
-        (map #(hash-map :name % :x (rand-int 500) :y (rand-int 500)) names)))
+    (let [names ["Slipper" "Allegra" "Squeaky" "Sarah Jane" "Totoro"
+        ]]
+        (map #(hash-map 
+            :name % 
+            :x (rand-int 500) 
+            :y (rand-int 500)
+            :velocity {:r 0 :angle 0}) 
+        names)))
 
 (defonce app-state (atom {
     :critters (init-critters)
@@ -21,12 +27,19 @@
 
 (defn to-random-nearby [it] 
     (let [{:keys [x y]} it]        
-        (assoc it :x (+ (rand-center 5) x) 
-                  :y (+ (rand-center 5) y))))
+        (assoc it :x (+ (rand-center 10) x) 
+                  :y (+ (rand-center 10) y))))
 
-(defn center-of-points [ps] 
-    {:x (mean (map :x ps))
-     :y (mean (map :y ps))})
+(defn mean-hash [xs xs-keys]    
+    (reduce 
+        (fn [coll k] (assoc coll k (mean (map k xs)))) 
+        {} xs-keys))
+
+(defn center-of-points [ps]
+    (mean-hash ps [:x :y]))
+
+(defn average-velocity [cs]
+    (mean-hash (map :velocity cs) [:r :angle]))
 
 (defn to-point-fn [f] (fn [a b] 
     (assoc a :x (f (:x a) (:x b))
@@ -42,28 +55,79 @@
 
 (def add-point (to-point-fn +))
 (def subtract-point (to-point-fn -))
+(def gt-point (to-point-fn >))
 
 (defn abs-point [a] 
-    assoc a :x (Math/abs (:x a))
-            :y (Math/abs (:y a)))
+    (assoc a :x (Math/abs (:x a))
+             :y (Math/abs (:y a))))
 
 (defn move-towards [r start end]
-    (let [angle (:angle (cartesian->polar (subtract-point start end)))
-          diff (polar->cartesian {:angle angle :r r})]
-        (subtract-point start diff)))
+    (let [  route       (cartesian->polar (subtract-point start end))
+            velocity    (assoc route :r (min (:r route) r))
+            with-v      (assoc start :velocity velocity)
+            diff        (polar->cartesian velocity)]
+        (subtract-point with-v diff)))
 
-(defn move-critters! [] 
-    (let [{:keys [width height critters]} @app-state
-          critter-target (center-of-points critters)
-          move-towards-target #(move-towards 10 % critter-target)
-          next-critters (map move-towards-target critters)]
+(defn critter-collision? [c1 c2]
+    (let [  diff            (abs-point (subtract-point c1 c2))
+            space           {:x 40 :y 40}
+            {:keys [x y]}   (gt-point space diff)]
+        (and (not= c1 c2) (and x y))))
+
+(defn exclude [x xs] (filter #(not= % x) xs))
+
+(defn rule-to-center [c cs]
+    (let [  other-critters  (exclude c cs)
+            other-center    (center-of-points other-critters)]
+        (move-towards 5 c other-center)))
+
+(defn rule-avoid-walls [c width height]
+    (let [  {:keys [x y]}   c]
+        (cond   (< x 0)         (assoc c :x 0)
+                (< y 0)         (assoc c :y 0)
+                (< width x)     (assoc c :x width)
+                (< height y)    (assoc c :y height)
+                :else c)))
+
+(defn rule-avoid-collisions [c cs]
+    (let [  collisions      (filter #(critter-collision? % c) cs)
+            collision-area  (center-of-points collisions)
+            no-collision?   (empty? collisions)]
+        (cond   
+            no-collision? c
+            :else   (move-towards -10 c collision-area))))
+
+(defn rule-match-velocity [c cs]
+    (let [  other-critters  (exclude c cs)
+            avg-velocity    (average-velocity other-critters)
+            r               (:r avg-velocity)
+            velocity        (assoc avg-velocity :r (/ r 4))
+            with-v          (assoc c :velocity velocity)
+            diff            (polar->cartesian velocity)]
+        (subtract-point with-v diff)))
+
+(defn critter-destination [c state]
+    (let [  cs  (:critters state)]
+        (-> c
+            (rule-to-center cs)
+            (rule-avoid-walls (:width state) (:height state))
+            (rule-avoid-collisions cs)
+            (rule-match-velocity cs)
+            )))
+
+
+(defn move-critters! [state] 
+    (let [  {:keys [width height critters]} state
+            next-critters (map #(critter-destination % state) critters)]
         (swap! app-state assoc :critters next-critters)))
 
 (defn app-loop! [] 
     (js/setTimeout (fn [] 
-        (move-critters!)
+        (move-critters! @app-state)
         (app-loop!)) 
     100))
+
+
 
 
 (defn translate [x y] (str "translate(" x "px," y "px)"))
@@ -74,7 +138,7 @@
 (defn module-critter [critter] 
     (let [{:keys [x y]} critter]
         [:g.module-critter 
-            {:style {:transition "transform 100ms"
+            {:style {:transition "transform 500ms"
                      :transform (translate x y)}}
             [:circle {:r 20 :style {:fill "brown"}}]]))
 
