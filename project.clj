ff755c7863997d502606ba7230c8827ed0aaf4b7
Justin Falcone <stanulam59@gmail.com>
2016-03-23 19:31:24 -0400

    upgrade cljs

--- project.clj
+++ project.clj
@@ -1,54 +1,50 @@
-(defproject critter-simulator "0.1.0-SNAPSHOT"
-  :description "guinea pig village simulation"
-  :url "http://justinfalcone.com/critter-simulator"
-  :license {:name "MIT"
-            :url "http://opensource.org/licenses/MIT"}
+(defproject hello-world "0.1.0-SNAPSHOT"
+    :description "guinea pig village simulation"
+    :url "http://justinfalcone.com/critter-simulator"
+    :license {:name "MIT"
+              :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
-                 [figwheel "0.2.5-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [reagent "0.5.0-alpha3"]]
+  :min-lein-version "2.6.1"
 
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-kibit "0.0.8"]
-            [lein-bikeshed "0.2.0"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.34"]
+                 [org.clojure/core.async "0.2.374"
+                  :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.5.1"]]
+
+  :plugins [[lein-figwheel "0.5.1"]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src" "dev_src" "test"]
-              :compiler {:output-to "resources/public/js/compiled/critter_simulator.js"
-                         :output-dir "resources/public/js/compiled/out"
-                         :optimizations :none
-                         :main critter-simulator.dev
-                         :asset-path "js/compiled/out"
-                         :source-map true
-                         :source-map-timestamp true
-                         :cache-analysis true }}
-             {:id "local"
-              :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/critter_simulator.js"
-                         :main critter-simulator.core
-                         :optimizations :none
-                         :pretty-print false}}
-             {:id "min"
-              :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/critter_simulator.js"
-                         :static-fns true
-                         :main critter-simulator.core
-                         :optimizations :advanced
-                         :pretty-print false}}]
-    :test-commands {"unit-tests" ["slimerjs" :runner
-                                  "target/cljs/testable.js"]}}
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
 
-  :figwheel {
-             :http-server-root "public" ;; default and assumes "resources"
-             :server-port 3449 ;; default
+                ;; If no code is to be run, set :figwheel true for continued automagical reloading
+                :figwheel {:on-jsload "hello-world.core/on-js-reload"}
+
+                :compiler {:main hello-world.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/critter_simulator.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/critter_simulator.js"
+                           :main hello-world.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
@@ -56,9 +52,9 @@
 
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is simple ring servers, if this
+             ;; server, this is for simple ring servers, if this
              ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
+             ;; :ring-handler critter_simulator.server/handler
 
              ;; To be able to open files in your editor from the heads up display
              ;; you will need to put a script on your path.
